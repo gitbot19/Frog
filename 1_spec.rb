76a6dd89d30c6c3ae875ef3b9d781e0ea8c065fe
John Doe <John.Doe@example.com>
2021-07-21 20:12:03 +0100

    1_spec.rb modified

--- 1_spec.rb
+++ 1_spec.rb
@@ -3,6 +3,6 @@ control "cis-1-2-1" do
   title "1.2.1 Verify CentOS GPG Key is Installed (Scored)"
   desc "CentOS cryptographically signs updates with a GPG key to verify that they are valid."
   describe command('rpm -q --queryformat "%{SUMMARY}\n" gpg-pubkey') do
-   its('stdout') { should match /CentOS 6 Official Signing Key/}
+   its('stdout') { should match /CentOS Official Signing Key/}
   end
 end
